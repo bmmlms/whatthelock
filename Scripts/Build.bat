74c1d84882594b05fc8540a20b46dbd3adf4ad6c
bmmlms <bmmlms@users.noreply.github.com>
2023-07-28 11:13:08 +0200

    Improved build script

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -18,22 +18,22 @@ if exist ..\Build\ (
 REM Build dlls
 cd ..\WhatTheLock
 lazbuild --build-all --cpu=x86_64 --os=Win64 --build-mode=%MODE% WhatTheLock.lpi
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 
 type ..\Build\%MODE%\x86_64\WhatTheLock.dll | "%PLINK%" -batch gaia osslsigncode-sign.sh > ..\Build\%MODE%\x86_64\WhatTheLock-signed.dll
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 move /y ..\Build\%MODE%\x86_64\WhatTheLock-signed.dll ..\Build\%MODE%\x86_64\WhatTheLock.dll
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 
 REM Build setup
 cd ..\WhatTheLock_Setup
 lazbuild --build-all --cpu=x86_64 --os=Win64 --build-mode=%MODE% WhatTheLock_Setup.lpi
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 
 type ..\Build\%MODE%\x86_64\WhatTheLock_Setup.exe | "%PLINK%" -batch gaia osslsigncode-sign.sh > ..\Build\%MODE%\x86_64\WhatTheLock_Setup-signed.exe
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 move /y ..\Build\%MODE%\x86_64\WhatTheLock_Setup-signed.exe ..\Build\%MODE%\x86_64\WhatTheLock_Setup.exe
-IF ERRORLEVEL 1 GOTO FAIL
+if %ERRORLEVEL% GEQ 1 GOTO FAIL
 
 ECHO.
 ECHO Build finished
